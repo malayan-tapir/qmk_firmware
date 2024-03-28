4702e4a7d85311e2bffc57ab4ccf8d5b18b162a1
malayan-tapir <malayan.tapir.333@gmail.com>
2024-03-28 22:25:14 +0900

    ideakeys_2

--- keyboards/ideakeys/keymaps/default/keymap.c
+++ keyboards/ideakeys/keymaps/default/keymap.c
@@ -38,25 +38,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(_LAYER1, KC_ESC),   KC_P7,   KC_P8,    KC_P9,    KC_TAB,
         LT(_LAYER2, KC_DEL),   KC_P4,   KC_P5,    KC_P6,    KC_BSPC,
         LT(_LAYER3, KC_HOME),  KC_P1,   KC_P2,    KC_P3,    
-        KC_P0,                 KC_PEQL, KC_PDOT,  KC_PENT 
+        KC_P0,                 KC_PEQL, KC_PDOT,  KC_PENT   
     ),
     [_LAYER1] = LAYOUT(
-        XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    KC_PGUP,
-        XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    KC_PGDN,
-        XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    
-        XXXXXXX,              XXXXXXX,    XXXXXXX,    XXXXXXX
+        _______,   _______,   _______,    _______,    KC_PGUP,
+        _______,   _______,   _______,    _______,    KC_PGDN,
+        _______,   _______,   _______,    _______,    
+        _______,              _______,    _______,    _______
     ),
     [_LAYER2] = LAYOUT(
-        XXXXXXX,   XXXXXXX,   KC_PSLS,    KC_PAST,    KC_LEFT,
-        XXXXXXX,   XXXXXXX,   KC_PMNS,    KC_PPLS,    KC_RGHT,
-        XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    
-        XXXXXXX,              XXXXXXX,    XXXXXXX,    XXXXXXX
+        _______,   _______,   KC_PSLS,    KC_PAST,    KC_LEFT,
+        _______,   _______,   KC_PMNS,    KC_PPLS,    KC_RGHT,
+        _______,   _______,   _______,    _______,    
+        _______,              _______,    _______,    _______
     ),
     [_LAYER3] = LAYOUT(
-        XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    KC_KB_VOLUME_UP,
-        XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    KC_KB_VOLUME_DOWN,
-        XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    
-        XXXXXXX,              XXXXXXX,    XXXXXXX,    XXXXXXX
+        _______,   _______,   _______,    _______,    KC_KB_VOLUME_UP,
+        _______,   _______,   _______,    _______,    KC_KB_VOLUME_DOWN,
+        _______,   _______,   _______,    _______,    
+        _______,              _______,    _______,    _______
     )
 };
 
